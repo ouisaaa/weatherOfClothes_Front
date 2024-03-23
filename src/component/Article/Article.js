9710166aaf664789ad7ec64b90b4e7db42135314
ouisaaa_pc <ouisaaa@gmail.com>
2024-03-24 03:43:55 +0900

    0324

--- src/component/Article/Article.js
+++ src/component/Article/Article.js
@@ -4,6 +4,8 @@ import { kakaoAPIDomain,dataDomain,cityList, kakaoAPIKey } from "../../config/co
 
 import Autocomplete from '@mui/material/Autocomplete';
 import TextField from '@mui/material/TextField';
+import { Grid } from "@mui/material";
+import Container from '@mui/material/Container';
 
 
 export default function Article(){
@@ -25,6 +27,8 @@ export default function Article(){
 
     //좌표를 통해 자연어 주소를 구하는 API
     function clickCurrentLocation(event){
+        event.preventDefault();
+
         if(event.target.textContent === "현제 위치 입력"){
             fetch(`${kakaoAPIDomain}?x=${geolocation.longitude}&y=${geolocation.latitude}`,{
                 method: "GET",
@@ -40,6 +44,7 @@ export default function Article(){
                 setDis(location.documents[1].region_2depth_name);
                 setNei(location.documents[1].region_3depth_name);
                 setIsLocation(false);
+                
             })
         }else{
             setIsLocation(true);
@@ -75,6 +80,17 @@ export default function Article(){
             }
         )
     }    
+
+    async function reset(e){
+        e.preventDefault();
+        Promise.all([setCity(''),setDis(''),setNei('')]).then(()=>setIsLocation(true))
+    }
+
+    // useEffect (()=>{
+    //     if(!isLocation){
+    //         setCity('');
+    //     }
+    // },[isLocation])
     return(
             <article>
                 <div>
@@ -86,63 +102,52 @@ export default function Article(){
                     <div>
                         <button onClick={check}/>
                     </div>
-                    {/* {isLocation ? ( */}
                         <>
                         <form>
-                    <button type={isLocation ? "reset" :null }onClick={clickCurrentLocation}>{isLocation ? "현제 위치 입력": "직접 위치 입력"}</button> 
+                    <button onClick={isLocation ? clickCurrentLocation :reset}>{isLocation ? "현제 위치 입력": "직접 위치 입력"}</button> 
                     <div>
                         <label>시</label>
                         <label>구</label>
                         <label>동</label>
                     </div>
                     <div>
-                        <div>
-                            <Autocomplete
-                                freeSolo
-                                options={cityList.map((option) => option)}
-                                renderInput={(params) => <TextField {...params} label="시/도"/>}
-                                onChange={distrctListSearch}
-                                value={isLocation ? null : city} readOnly={!isLocation}
-                            />
-                            <Autocomplete
-                                freeSolo
-                                options={disList.map((option) => option)}
-                                renderInput={(params) => <TextField {...params} label="시/구/군"/>}
-                                onChange={neighborhoodListSearch}
-                                value={isLocation ?  null:dis} readOnly={!isLocation}
-                            />
-                            <Autocomplete
-                                freeSolo
-                                options={neiList.map((option) => option)}
-                                renderInput={(params) => <TextField {...params} label="동/읍/리"/>}
-                                value={isLocation ?  null:nei} readOnly={!isLocation}
-                            />
-                        </div>
-                        <input type="text" className="input" ref={region_1depth_name} value={isLocation ? null : city} readOnly={!isLocation}/>
-                        <input type="text" className="input"ref={region_2depth_name} value={isLocation ?  null:dis} readOnly={!isLocation}/>
-                        <input type="text" className="input"ref={region_3depth_name} value={isLocation ?  null:nei} readOnly={!isLocation}/>
-                    
+                    <Container maxWidth="xm">
+                            <Grid container spacing={3}>
+                                <Grid item xs={4}>
+                                <Autocomplete
+                                    freeSolo
+                                    options={cityList.map((option) => option)}
+                                    renderInput={(params) => <TextField {...params} label="시/도"/>}
+                                    onChange={distrctListSearch}
+                                    value={isLocation ? null : city} readOnly={!isLocation}
+                                    ref={region_1depth_name}
+                                />
+                                </Grid>
+                                <Grid item xs={4}>
+                                <Autocomplete
+                                    freeSolo
+                                    options={disList.map((option) => option)}
+                                    renderInput={(params) => <TextField {...params} label="시/구/군"/>}
+                                    onChange={neighborhoodListSearch}
+                                    value={isLocation ?  null:dis} readOnly={!isLocation}
+                                    ref={region_2depth_name}
+                                />
+                                </Grid>
+                                <Grid item xs={4}>
+                                <Autocomplete
+                                    freeSolo
+                                    options={neiList.map((option) => option)}
+                                    renderInput={(params) => <TextField {...params} label="동/읍/리"/>}
+                                    value={isLocation ?  null:nei} readOnly={!isLocation}
+                                    ref={region_3depth_name} 
+                                />
+                                </Grid>
+                            </Grid>
+                        </Container>
                     </div>
                     </form>
                     <button>검색</button>
                     </>
-                    {/* ):(
-                        <>
-                        <button onClick={clickCurrentLocation}></button> 
-                        <div>
-                            <label>시</label>
-                            <label>구</label>
-                            <label>동</label>
-                        </div>
-                        <div>
-                            <input type="text" value={city}/>
-                            <input type="text" value={dis}/>
-                            <input type="text" value={nei}/>
-                            
-                        </div>
-                        <button>검색</button>
-                        </>
-                    )} */}
                 </div>
             </article>
     );
